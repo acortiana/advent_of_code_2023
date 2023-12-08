b47ba78188c068fad0667e97cceb80f73dbe9b11
Alessandro Cortiana <alessandro.cortiana@gmail.com>
2023-12-08 11:24:36 +0100

    day6

---
+++ 06-part1.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"bufio"
+	"os"
+	"strings"
+	"strconv"
+)
+
+func atoi(textnumber string) int {
+	tmpvalue, _:= strconv.Atoi(textnumber)
+	return tmpvalue
+}
+
+func read_input() ([][]int) {
+	var result_a [][]int
+	var result_b [][]int
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		line := scanner.Text()
+		var tmp_a []int
+		for _, textnumber := range strings.Fields(line)[1:] {
+			tmp_a = append(tmp_a,atoi(textnumber))
+		}
+		result_a = append(result_a,tmp_a)
+	}
+	for i := 0; i < len(result_a[0]); i++ {
+		result_b = append(result_b,[]int{result_a[0][i],result_a[1][i]})
+	}
+	return result_b
+}
+
+func better_races_count(race *[]int) int {
+	ms := (*race)[0]
+	best := (*race)[1]
+	mybestcount := 0
+	for i:=1; i < ms; i++ {
+		result := (ms - i) * i
+		if result > best {
+			mybestcount++
+		}
+	}
+	return mybestcount
+}
+
+func main() {
+	races := read_input()
+	result := 1
+	for _, race := range races {
+		result *= better_races_count(&race)
+	}
+	fmt.Println(result)
+}
